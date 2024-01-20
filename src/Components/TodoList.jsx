ff06dcfcd00b8652ab2797cc23e5cf87ff5f05c0
Murad-Takla <irfanchowdhurycr7@gmail.com>
2024-01-20 11:35:56 +0600

    new todo add some feature(notes , title , add subtask and manage subtask) added

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,16 +1,18 @@
 import React, { useContext, useState } from 'react';
 import IconNotes from '../Icons/IconNotes';
+import IconPlus from '../Icons/IconPlus';
 import { TodosContext } from '../contexts/TodosContext';
+import { v4 as uuidv4 } from 'uuid';
 
 const TodoList = () => {
 
 
-    const {todos} = useContext(TodosContext)
+    const { todayTodo } = useContext(TodosContext)
 
     return (
         <div>
             {
-                todos.map((todo ) => {
+                todayTodo.map((todo) => {
                     return (
                         <Todo todo={todo} key={todo.id} ></Todo>
                     );
@@ -21,9 +23,10 @@ const TodoList = () => {
 };
 
 
-const Todo = ({todo}) => {
+const Todo = ({ todo }) => {
 
-    
+
+    const title = todo.title
 
     const [isExpanded, setIsExpanded] = useState(false)
 
@@ -33,69 +36,135 @@ const Todo = ({todo}) => {
                 onDoubleClick={() => { setIsExpanded(!isExpanded) }}
             >
                 <input type="checkbox" className='scale-125' name='name' id='id' />
-                <span className='ml-1 text-gray-600'>{todo.title}</span>
+
+                {
+                    title.length === 0 ? <span value='New todo '
+                        className="ml-1 text-gray-400"
+                    >New todo</span> : <span className='ml-1 text-gray-600'>{todo.title}</span>
+                }
             </div>
         );
     }
-    return <TodoExpanded  todo = {todo} setIsExpanded = {setIsExpanded} isExpanded ={isExpanded}/>
+    return <TodoExpanded todo={todo} setIsExpanded={setIsExpanded} isExpanded={isExpanded} />
 }
 
 
-const TodoExpanded = ({todo , setIsExpanded , isExpanded }) => {
+const TodoExpanded = ({ todo, setIsExpanded, isExpanded }) => {
 
-    const [title , setTitle] =  useState (todo.title)
+    const [title, setTitle] = useState(todo.title)
+    const [notes, setNotes] = useState(todo.notes)
 
-    const {dispatch } = useContext(TodosContext)
 
-   
-   
+
+
+
+    const { dispatch } = useContext(TodosContext)
+
+
+
 
     return (
         <div className="bg-white rounded-md shadow-md p-4 mb-2">
 
+            {/* Title */}
 
             <div className="flex items-center py-2 border-b">
                 <input className="scale-125" type="checkbox" name="name" id="id" />
-              
-                    
-
                 <input
-                  value={title}
-                 onChange={(e) => { setTitle(e.target.value) }}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                            dispatch({ type: "UPDATE TODO",   id : todo.id ,  title })
-                        }
+                    defaultValue={title}
+
+                    onChange={(e) => {
+                        setTitle(e.target.value)
+                        dispatch({ type: "UPDATE TODO", id: todo.id, title: e.target.value })
                     }}
                     className="border-none focus:outline-none ml-2"
                     placeholder="New To-Do"
                     type="text"
                     aria-autocomplete="list"
                 />
-                <button 
-                onClick={ () =>setIsExpanded(false)}
-                className='border px-1 rounded font-medium'>Collapse</button>
+                <button
+                    onClick={() => setIsExpanded(false)}
+                    className='border px-1 rounded font-medium'>Collapse</button>
             </div>
+
+
+            {/* Notes */}
+
             <div className="flex items-center py-2 border-b">
 
-                <IconNotes width={15}></IconNotes>
+                <IconNotes width={15} height={15}></IconNotes>
                 <input
+
+                    defaultValue={todo.notes}
                     className="border-none focus:outline-none ml-2 text-sm"
                     placeholder="Notes"
                     type="text"
+                    onChange={(e) => {
+                        dispatch({ type: "UPDATE TODO", id: todo.id, title: title, notes: e.target.value })
+                    }}
+
                 />
             </div>
-            <div className="py-2 border-b">
+
+            {/* SubTasks */}
+
+
+            <div className='mb-2'>
+                <ul>
+                    {
+                        todo.subtask.map((sb, idx) =>{
+                            return (
+                            <li key={idx} className='text-sm text-gray-400'>
+                               
+                              <button 
+                              onClick={()=>{
+                                //   console.log(sb.id)
+                                //   console.log(sb.isCompleted)
+                                  dispatch({
+                                    type : "UPDATE SUBTASK",
+                                    todoId : todo.id,
+                                    subtaskId : sb.id,
+                                    isCompleted : !sb.isCompleted,
+                                    
+                                })
+                              }}
+                              className='w-[10px] h-[10px] border rounded-full border-gray-400'></button>
+                                <span> <input className='border-none focus:outline-none ml-2 text-sm' type="text" defaultValue={sb.value} /></span>
+                               
+                            </li>
+                            )
+                        }
+                            
+                        )
+                    }
+                </ul>
+            </div>
+            <div className="flex items-center py-2 border-b">
+
+                <IconPlus width={16} ></IconPlus>
                 <ul className="mb-4">
                 </ul>
                 <div className="flex items-center">
                     <input
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                dispatch({
+                                    type: 'ADD_SUBTASK',
+                                    id: todo.id,
+                                    value: e.target.value
+                                })
+                                e.target.value = ''
+                            }
+                        }}
                         className="border-none focus:outline-none ml-2 text-sm"
                         placeholder="New Sub Task"
                         type="text"
                     />
                 </div>
             </div>
+
+            {/* Add Tags */}
+
             <div className="flex items-center py-2 border-b">
 
                 <input
@@ -104,6 +173,9 @@ const TodoExpanded = ({todo , setIsExpanded , isExpanded }) => {
                     type="text"
                 />
             </div>
+
+            {/* Set When */}
+
             <div className="flex items-center py-2 border-b">
 
                 <input
@@ -112,6 +184,9 @@ const TodoExpanded = ({todo , setIsExpanded , isExpanded }) => {
                     type="text"
                 />
             </div>
+
+            {/* Inbox */}
+
             <div className="flex items-center py-2 border-b">
 
                 <input
@@ -120,6 +195,9 @@ const TodoExpanded = ({todo , setIsExpanded , isExpanded }) => {
                     type="text"
                 />
             </div>
+
+            {/* Add deadline  */}
+
             <div className="flex items-center py-2">
                 <input
                     className="border-none focus:outline-none ml-2 text-sm"
--- src/contexts/TodosContext.jsx
+++ src/contexts/TodosContext.jsx
@@ -2,66 +2,142 @@ import { createContext, useReducer } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 
-export const TodosContext =  createContext('')
+export const TodosContext = createContext('')
 
 
-export const TodoProvider = ({children}) =>{
+export const TodoProvider = ({ children }) => {
 
     const reducer = (state, action) => {
-       
 
         if (action.type === 'ADD TODO') {
             const newState = [...state]
             newState.push({
                 id: uuidv4(),
-                title: action.title
+                title: action.title,
+                notes: "",
+                subtask: [],
+                tags: [],
+                date: new Date(),
+                deadline: ''
+
             })
             return newState
         }
-        
-      
-        if (action.type ===  "UPDATE TODO"){
-            return state.map((todo) =>  {
-                if(todo.id === action.id){
+
+        if (action.type === "UPDATE TODO") {
+            return state.map((todo) => {
+                console.log(action)
+                if (todo.id === action.id) {
+
                     return ({
-                        id : action.id,
-                        title : action.title
+                        id: action.id,
+                        title: action.title,
+                        notes: action.notes,
+                        subtask: [],
+                        tags: [],
+                        date: new Date(),
+                        deadline: ''
+
                     })
                 }
                 return todo
 
             })
-                
+
         }
 
+        if (action.type === 'ADD_SUBTASK') {
+            return state.map((todo) => {
+                if (todo.id === action.id) {
+                    const newSubTask = [...todo.subtask, {id: uuidv4() , isCompleted : false , value : action.value}]
 
+                    return {
+
+                        ...todo,
+                        
+                        subtask: newSubTask
+                    }
+                }
+
+                return todo
+            })
+        }
+
+        if(action.type === 'UPDATE SUBTASK'){
+
+            return state.map((todo) => {
+                // console.log(todo)
+
+                console.log(action.isCompleted)
+                if (todo.subtask.id === action.id) {
+                    const newSubTask = [...todo.subtask, {id:action.subtaskId , isCompleted : action.isCompleted , value : action.value}]
+
+                    return {
+                        ...todo,    
+                        subtask: newSubTask
+                    }
+                }
+
+                return todo
+            })
+        }
 
         return state
-
     }
 
     const [todos, dispatch] = useReducer(reducer, [
         {
             id: uuidv4(),
-            title: "Learn HTML"
+            title: "Learn HTML",
+            notes: "",
+            subtask: [],
+            tags: [],
+            date: new Date("2024-01-20"),
+            deadline: ''
         },
         {
             id: uuidv4(),
-            title: "Learn CSS"
+            title: "Learn CSS",
+            notes: "",
+            subtask: [],
+            tags: [],
+            date: new Date(),
+            deadline: ''
         },
         {
             id: uuidv4(),
-            title: "Learn JS"
+            title: "Learn JS",
+            notes: "",
+            subtask: [],
+            tags: [],
+            date: new Date(),
+            deadline: ''
+
         }
 
     ])
 
-     
+    const checkToday = ({ date }) => {
 
-   console.log(todos)
+        const todayDate = new Date()
 
+        const todoYear = date.getFullYear()
+        const todoMonth = date.getMonth() + 1    /// month is always 0 based Index (so you to add '1' after month if you want proper month using Date() )
+        const todoDay = date.getDate()
+
+        return (
+            todayDate.getFullYear() === todoYear &&
+            todayDate.getMonth() + 1 === todoMonth &&
+            todayDate.getDate() === todoDay
+        )
+
+    }
+    const todayTodo = todos.filter(checkToday)
+
+
+    // console.log(todos)
     return (
-        <TodosContext.Provider value={{todos , dispatch}}>
+        <TodosContext.Provider value={{ todayTodo, todos, dispatch }}>
             {children}
         </TodosContext.Provider>
     );
